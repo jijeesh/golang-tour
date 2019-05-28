e7f69211b401908395dc0472c3784a0f2d2fe77b
Jijeesh <jijeesh.ka@gmail.com>
2019-05-28 12:42:19 +0530

    create function

--- docker/stack.go
+++ docker/stack.go
@@ -15,39 +15,51 @@ func main() {
 	for {
 		fmt.Print("DockerShell-> ")
 		// Read the keyboad input
-		input, _ := reader.ReadString('\n')
-		// convert CRLF to LF
-		input = strings.Replace(input, "\n", "", -1)
-
-		if strings.Compare("hi", input) == 0 {
-			fmt.Println("hello, Yourself")
-		}
-
-		docker := "docker"
-		args := input
-		cmd := exec.Command(docker, args)
-		cmdReader, err := cmd.StdoutPipe()
-
+		input, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error creating StdoutPipe for Cmd", err)
-			os.Exit(1)
+			fmt.Fprintln(os.Stderr, err)
 		}
-		scanner := bufio.NewScanner(cmdReader)
-		go func() {
-			for scanner.Scan() {
-				fmt.Printf("docker %s out | %s\n", input, scanner.Text())
-			}
-		}()
-		err = cmd.Start()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error starting for Cmd", err)
-			os.Exit(1)
-		}
-		err = cmd.Wait()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error waiting for Cmd", err)
-			os.Exit(1)
+		// Handle the execution of the input
+		if err = execInput(input); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+
 		}
 
 	}
+}
+
+func execInput(input string) error {
+	// convert CRLF to LF
+	input = strings.Replace(input, "\n", "", -1)
+
+	if strings.Compare("hi", input) == 0 {
+		fmt.Println("hello, Thank you for using Docker")
+	}
+
+	docker := "docker"
+	args := input
+	cmd := exec.Command(docker, args)
+	cmdReader, err := cmd.StdoutPipe()
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error creating StdoutPipe for Cmd", err)
+		os.Exit(1)
+	}
+	scanner := bufio.NewScanner(cmdReader)
+	go func() {
+		for scanner.Scan() {
+			fmt.Printf("docker %s out | %s\n", input, scanner.Text())
+		}
+	}()
+	err = cmd.Start()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error starting for Cmd", err)
+		os.Exit(1)
+	}
+	err = cmd.Wait()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error waiting for Cmd", err)
+		os.Exit(1)
+	}
+	return err
 }
